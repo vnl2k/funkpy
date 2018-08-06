f38b75ea34a3f75ed0f45aee81bb05af2f28dbfc
vnl2k <38137627+vnl2k@users.noreply.github.com>
2018-08-06 10:55:11 +0100

    v1.0.1: minor changes

--- build-pypi.py
+++ build-pypi.py
@@ -1,10 +1,14 @@
 import os
 from shutil import rmtree, move
-from glob import glob
 
 PACKAGE_NAME = "funkpy"
 
 os.system("python3 setup.py sdist bdist_wheel")
 rmtree("./" + PACKAGE_NAME + ".egg-info")
 rmtree("./build")
+
+# test.pypi.org
 os.system('python3 -m twine upload --repository-url "https://test.pypi.org/legacy/" "dist/*"')
+
+# live
+os.system('python3 -m twine upload "dist/*"')
